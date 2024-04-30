22a775a44a7c667820c07e328b199f0289511f62
Pavel Danilov <danilov211@mail.ru>
2024-04-30 18:29:29 +0300

    add hw2

---
+++ src/main/java/ru/sh/g/hw2/service/UserService.java
@@ -0,0 +1,39 @@
+package ru.sh.g.hw2.service;
+
+import ru.sh.g.hw2.model.User;
+import ru.sh.g.hw2.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
+
+    public User getOneById(int id){
+        return userRepository.getOne(id);
+    }
+
+    public void updateUser(User user){
+        userRepository.update(user);
+    }
+}
